6e420bb07f5112016310192d30caa198559853ca
jessie_624588 <jessie_624588@naver.com>
2021-11-03 12:02:30 +0900

    2021-11-03

---
+++ flaskhotpot/routes.py
@@ -0,0 +1,128 @@
+from flask import render_template, url_for, flash, redirect, request, abort
+from flaskhotpot import app, db, bcrypt
+from flaskhotpot.forms import RegistrationForm, LoginForm, PostForm
+from flaskhotpot.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
+
+
+@app.route("/category", methods=["GET", "POST"])
+def category():
+    posts = Post.query.all()
+    return render_template("category.html", title="Category", posts=posts)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode(
+            "utf-8"
+        )
+        user = User(
+            username=form.username.data, email=form.email.data, password=hashed_password
+        )
+        db.session.add(user)
+        db.session.commit()
+        flash("계정을 생성되었습니다! 이제 로그인할 수 있습니다", "success")
+        return redirect(url_for("login"))
+    return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get("next")
+            return redirect(next_page) if next_page else redirect(url_for("home"))
+        else:
+            flash("로그인에 실페했습니다. 아이디나 비밀번호를 다시 확인해주시기 바랍니다", "danger")
+    return render_template("login.html", title="Login", form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
+
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template("account.html", title="Account")
+
+
+@app.route("/post/new", methods=["GET", "POST"])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(
+            title=form.title.data,
+            content=form.content.data,
+            author=current_user,
+            category_id=form.region.data,
+        )
+        db.session.add(post)
+        db.session.commit()
+        flash("작성하신 글을 생성되었습니다!", "success")
+        return redirect(url_for("home"))
+    return render_template(
+        "create_post.html", title="New Post", form=form, legend="글 작성"
+    )
+
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template("post.html", title=post.title, post=post)
+
+
+@app.route("/post/<int:post_id>/update", methods=["GET", "POST"])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash("작성하신 글을 업데이트 되었습니다!", "success")
+        return redirect(url_for("post", post_id=post.id))
+    elif request.method == "GET":
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template(
+        "create_post.html", title="update Post", form=form, legend="글 내용 업데이트"
+    )
+
+
+@app.route("/post/<int:post_id>/delete", methods=["POST"])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash("글 성공적으로 삭제되었습니다!", "success")
+    return redirect(url_for("home"))
